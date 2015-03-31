b16d14aec047890b109e78742ffb178e47510169
XanMead <x.m.mead@gmail.com>
2015-03-31 13:21:26 -0400

    Implemented add function, added prototypes for other methods

--- js/app.js
+++ js/app.js
@@ -1,22 +1,63 @@
-$(document).ready(function() {
-
-});
-
 var handle = '<div class="handle"><i class="fa fa-bars"></i></div>';
 var checkYes = '<div class="button checkbox"><i class="fa fa-check"></i></div>';
 var checkNo = '<div class="button check-o"><i class="fa fa-circle-o"></i></div>';
+var editBox = '<input class="item-entry edit-box" id="edit" type="text">';
 var edit = '<div class="button edit"><i class="fa fa-pencil"></i></div>';
 var delButton = '<div class="button delete"><i class="fa fa-remove"></i></div>';
 
+$(document).ready(function() {
+
+	// Allow enter to trigger add button click event
+	// NOT YET FUNCTIONAL
+	$('.item-entry').keypress(function(event) {
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			console.log(event.target.next());
+			event.target.next().click();
+		}
+	});
+
+	// Add button listener
+	$('.add-button').on('click', function(event) {
+		addItem();
+	});
+
+	// Check button listener
+	$('.checkbox').on('click', function(event) {
+		checkItem(event);
+	});
+
+	// Uncheck button listener
+	$('.check-o').on('click', function(event) {
+		uncheckItem(event);
+	});
+	
+	// Delete button listener
+	$('.delete').on('click', function(event) {
+		deleteItem(event);
+	});
+	
+	// Edit button listener
+	$('.edit').on('click', function(event) {
+		console.log("Edit function not yet implemented...");
+	});
+	
+});
+
+
 function addItem() {
-	var itemText = $('.item-entry').val().trim();
+	console.log("adding item...");
+	var itemText = $('#add').val().trim();
 	if (!itemText) {
+		// String is empty/pure whitespace
 		alert('Please enter text to add an item.')
 	}
 	else {
 		var entry = createEntry(itemText);
 		$('<li class="item-box"></div>').prependTo('.item-list').html(entry);
 	}
+	// Clear input
+	$('#add').val('');
 }
 
 function createEntry(itemText) {
@@ -24,6 +65,18 @@ function createEntry(itemText) {
 	entry += '<div class="item-display unchecked">' + itemText + '</div>';
 	entry += edit + delButton;
 	return entry;
+}
+
+function checkItem(event) {
+	console.log("Check function not yet implemented...");
+}
+
+function uncheckItem(event) {
+	console.log("Uncheck function not yet implemented...");
+}
+
+function deleteItem(event) {
+	console.log("Delete function not yet implemented...");
 }
 
 /*
@@ -44,6 +97,7 @@ OLD LIST STYLE
 					<i class="fa fa-circle-o"></i>
 				</div>
 				<div class="item-display unchecked">Bread</div>
+				<input class="item-entry" type="text" placeholder="Add item...">
 				<div class="button edit">
 					<i class="fa fa-pencil"></i>
 				</div>
