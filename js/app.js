a4c873f06677ff6547c8164bda55c4dc15d3bc2a
XanMead <x.m.mead@gmail.com>
2015-03-31 23:33:15 -0400

    implemented enter key listener; removed old comments

--- js/app.js
+++ js/app.js
@@ -8,18 +8,16 @@ var delButton = '<div class="button delete"><i class="fa fa-remove"></i></div>';
 $(document).ready(function() {
 
 	// Allow enter to trigger add button click event
-	// NOT YET FUNCTIONAL
 	$('.item-entry').keypress(function(event) {
 		if (event.keyCode == 13) {
 			event.preventDefault();
-			console.log(event.target.next());
-			event.target.next().click();
+			$(event.target).next().click();
 		}
 	});
 
 	// Initialize data listener
 	$('.logo').on('click', function(event) {
-		console.log('init...');
+		console.log('Adding init data');
 		initData();
 	});
 
@@ -106,115 +104,3 @@ function uncheckItem(event) {
 function deleteItem(event) {
 	console.log("Delete function not yet implemented...");
 }
-
-/*
-
-OLD LIST STYLE
-
-<div class="item-list">
-	<ul>
-		<li>
-			<div class="item-box">
-				<div class="handle">
-					<i class="fa fa-bars"></i>
-				</div>
-				<div class="button checkbox">
-					<i class="fa fa-check"></i>
-				</div>
-				<div class="button check-o">
-					<i class="fa fa-circle-o"></i>
-				</div>
-				<div class="item-display unchecked">Bread</div>
-				<input class="item-entry" type="text" placeholder="Add item...">
-				<div class="button edit">
-					<i class="fa fa-pencil"></i>
-				</div>
-				<div class="button delete">
-					<i class="fa fa-remove"></i>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="item-box">
-				<div class="handle">
-					<i class="fa fa-bars"></i>
-				</div>
-				<div class="button checkbox">
-					<i class="fa fa-check"></i>
-				</div>
-				<div class="button check-o">
-					<i class="fa fa-circle-o"></i>
-				</div>
-				<div class="item-display unchecked">Meat</div>
-				<div class="button edit">
-					<i class="fa fa-pencil"></i>
-				</div>
-				<div class="button delete">
-					<i class="fa fa-remove"></i>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="item-box">
-				<div class="handle">
-					<i class="fa fa-bars"></i>
-				</div>
-				<div class="button checkbox">
-					<i class="fa fa-check"></i>
-				</div>
-				<div class="button check-o">
-					<i class="fa fa-circle-o"></i>
-				</div>
-				<div class="item-display unchecked">hex bolts stainless steel 1&frac12;" - 30 x 6"</div>
-				<div class="button edit">
-					<i class="fa fa-pencil"></i>
-				</div>
-				<div class="button delete">
-					<i class="fa fa-remove"></i>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="item-box">
-				<div class="handle">
-					<i class="fa fa-bars"></i>
-				</div>
-				<div class="button checkbox">
-					<i class="fa fa-check"></i>
-				</div>
-				<div class="button check-o">
-					<i class="fa fa-circle-o"></i>
-				</div>
-				<div class="item-display checked">Gritted Teeth</div>
-				<div class="button edit">
-					<i class="fa fa-pencil"></i>
-				</div>
-				<div class="button delete">
-					<i class="fa fa-remove"></i>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="item-box">
-				<div class="handle">
-					<i class="fa fa-bars"></i>
-				</div>
-				<div class="button checkbox">
-					<i class="fa fa-check"></i>
-				</div>
-				<div class="button check-o">
-					<i class="fa fa-circle-o"></i>
-				</div>
-				<div class="item-display checked">Manly Spirit</div>
-				<div class="button edit">
-					<i class="fa fa-pencil"></i>
-				</div>
-				<div class="button delete">
-					<i class="fa fa-remove"></i>
-				</div>
-			</div>
-		</li>
-	</ul>
-</div>
-
-*/
