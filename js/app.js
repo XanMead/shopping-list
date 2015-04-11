b7e39d5757a07afd79eec35d2bb3393184c5c4a3
XanMead <x.m.mead@gmail.com>
2015-04-10 22:16:39 -0400

    .

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
+/* Item Display Components */
 var handle = '<div class="handle"><i class="fa fa-bars"></i></div>';
-var checkYes = '<div class="button checkbox" id=><i class="fa fa-check"></i></div>';
-var checkNo = '<div class="button check-o"><i class="fa fa-circle-o"></i></div>';
+var checkYes = '<div class="button checkbox"><i class="fa fa-check"></i></div>';
 var editBox = '<input class="item-entry edit-box" type="text">';
 var edit = '<div class="button edit"><i class="fa fa-pencil"></i></div>';
 var delButton = '<div class="button delete"><i class="fa fa-remove"></i></div>';
+
+var editing = false;
 
 $(document).ready(function() {
 
@@ -11,8 +13,12 @@ $(document).ready(function() {
 	$('.item-entry').keypress(function(event) {
 		if (event.keyCode == 13) {
 			event.preventDefault();
-			// Terget the next adjacent button, i.e 'add' or 'edit'
+			// Target the next adjacent button, i.e 'add' or 'edit'
 			$(event.target).next('.button').click();
+			if (event.target.id != 'add') {
+				console.log(event.target);
+				$(event.target).blur();
+			}
 		}
 	});
 
@@ -23,20 +29,30 @@ $(document).ready(function() {
 	$('.add-button').on('click', function(event) { addItem(); });
 
 	// Check button listener
-	$('.item-list').on('click', '.checkbox', function(event) {
-		console.log(event.target);
-		checkItem(event);
-	});
+	$('.item-list').on('click', '.checkbox', function(event) { toggleItem(event); });
 
 	// Uncheck button listener
 	$('.item-list').on('click', '.check-o', function(event) { uncheckItem(event); });
 	
 	// Delete button listener
 	$('.item-list').on('click', '.delete', function(event) { deleteItem(event); });
+
+	// Catch editing blur to hide edit-box.
+	$('.item-list').on('blur', '.edit-box', function(event) {
+		console.log('blur');
+		blurEditBox(event);
+	});
 	
 	// Edit button listener
 	$('.item-list').on('click', '.edit', function(event) {
-		console.log("Edit function not yet implemented...");
+		if (!editing) {
+			console.log('editing')
+			editItem(event);
+		}
+		else {
+			console.log('committing')
+			commitEdit(event);
+		}
 	});
 	
 	// How-to rollover listeners
@@ -69,31 +85,75 @@ function addItem() {
 	if (!itemText) {
 		// String is empty/pure whitespace
 		alert('Please enter text to add an item.')
+		// Clear input
+		$('#add').val('');
+	}
+	else if (itemText.length > 30) {
+		alert('Please limit your entry to 30 characters.')
 	}
 	else {
 		createEntry(itemText);
+		// Clear input
+		$('#add').val('');
 	}
-	// Clear input
-	$('#add').val('');
 }
 
 /* Creates an entry on the list */
 function createEntry(itemText) {
 	console.log("Creating item '" + itemText + "'...");
-	var entry = handle + checkYes + checkNo;
-	entry += '<div class="item-display" id="unchecked">' + itemText + '</div>';
-	entry += edit + delButton;
+	var entry = handle + checkYes;
+	entry += '<div class="item-display unchecked">' + itemText + '</div>';
+	entry += editBox + edit + delButton;
 	$('<li class="item-box"></div>').appendTo('.item-list').html(entry);
 }
 
-function checkItem(event) {
-	
-}
-
-function uncheckItem(event) {
-	console.log("Uncheck function not yet implemented...");
+function toggleItem(event) {
+	var itemBox = $(event.target).parents('.item-box');
+	// Toggle .unchecked [off -> on] or [on -> off]
+	itemBox.children('.item-display').toggleClass('unchecked');
+	// Toggle .checked on [on -> off] or [off -> on]
+	itemBox.children('.item-display').toggleClass('checked');
+	// Toggle check-button style
+	itemBox.children('.checkbox').toggleClass('button-toggled');
 }
 
 function deleteItem(event) {
 	$(event.target).parents('.item-box').remove();
+}
+
+function editItem(event) {
+	editing = true;
+
+	var itemBox = $(event.target).parents('.item-box');
+	var itemDisplay = itemBox.children('.item-display');
+	var editBox = itemBox.children('.edit-box');
+
+	itemBox.children('.edit').toggleClass('button-toggled');
+
+	// Populate input with current text
+	editBox.val(itemDisplay.text());
+
+	// Swap display for entry
+	itemDisplay.hide();
+	editBox.show();
+	editBox.focus();
+}
+
+function commitEdit(event) {
+	var itemBox = $(event.target).parents('.item-box');
+	var itemDisplay = itemBox.children('.item-display');
+	var editBox = itemBox.children('.edit-box');
+
+	itemDisplay.text(editBox.val());
+	editing = false;
+}
+
+function blurEditBox(event) {
+	var itemBox = $(event.target).parents('.item-box');
+	// Swap edit-box and item display
+	$(event.target).hide();
+	itemBox.children('.item-display').show();
+	// Detoggle edit button
+	itemBox.children('.edit').toggleClass('button-toggled');
+	editing = false;
 }
